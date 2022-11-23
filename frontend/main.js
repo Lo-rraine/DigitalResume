9815dfbc52883f93387040bd2120bee8f8affdf8
Lorrainem <moyolorraine98@gmail.com>
2022-11-23 12:57:00 +0200

    Added a loader for api

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,25 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
+const loader = document.getElementById('loader');
+const errorMsg = document.getElementById('error');
+function showLoader(){
+    loader.classList.add('show');
+}
+function hideLoader(){
+    loader.classList.remove('show');
+}
+function errorMessage(){
+    errorMsg.classList.add('error');
+}
+
 
 const functionApiUrl = 'https://getresumecounter20221122183757.azurewebsites.net/api/Function1?code=wP2R2IcRoOQl3_Od2xSH9ojMvYQPOOn-7lSk9KDt-XW7AzFurL2NKg==';
 
 
 const getVisitCount = () => {
+    showLoader();
+
     let count = 30;
     
     fetch(functionApiUrl).then(response => {
@@ -14,8 +28,13 @@ const getVisitCount = () => {
         console.log("Website called function API.");
         count =  response.count;
         document.getElementById("counter").innerText = count;
+        hideLoader();
     }).catch(function(error){
         console.log(error);
+        document.getElementById("counter").innerText = 10;
     });
     return count;
 }
+
+
+
